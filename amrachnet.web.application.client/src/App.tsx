9e9ab00dfc0536ee807899712b374662a4ad454d
Andmar201 <andreamartsia@gmail.com>
2026-01-01 05:31:36 +0100

    ROUTE CHANGES

--- amrachnet.web.application.client/src/App.tsx
+++ amrachnet.web.application.client/src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/amrach-net-frontend">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
